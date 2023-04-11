111a9dc0642f98555f2c9557eb20344ccd7de603
Eric Wallace <razzledazzle061980@gmail.com>
2023-04-10 20:29:04 -0400

    Add files via upload

---
+++ CS-465 Full Stack Development/PROJECT/CS-465 Project/app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { TripsService } from "../services/trips.service";
+import { ActivatedRoute, Router } from "@angular/router";
+
+@Component({
+  selector: 'app-edit-trip',
+  templateUrl: './edit-trip.component.html',
+  styleUrls: ['./edit-trip.component.css']
+})
+export class EditTripComponent implements OnInit{
+  editForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private tripsService: TripsService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.editForm = this.formBuilder.group({
+      _id: [''],
+      __v: [''],
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required],
+    })
+  }
+
+  ngOnInit(): void {
+    this.tripsService.getTrip(this.activatedRoute.snapshot.params['tripCode']).subscribe({
+      next: trip => this.editForm.patchValue(trip),
+      error: e => console.log(e)
+    });
+  }
+
+  updateTrip() {
+    if (this.editForm.valid) {
+      this.tripsService.updateTrip(this.activatedRoute.snapshot.params['tripCode'], this.editForm.value)
+        .subscribe( {
+          next: () => this.router.navigate(['/']),
+          error: e => console.log(e)
+        });
+    }
+  }
+
+  get f() { return this.editForm.controls; }
+}
+
